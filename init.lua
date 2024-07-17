205037323a88c63fb82b521d36f4e9146aed3c07
Mikhail Aksenov <mikhail.aksenov@zaptec.com>
2024-07-17 21:11:24 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("thecoldwine.remap")
+
+print("hello")
---
+++ lua/thecoldwine/packer.lua
@@ -0,0 +1,27 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use 'shaunsingh/nord.nvim'
+
+  use {
+	  'nvim-treesitter/nvim-treesitter',
+	  run = function()
+		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+		  ts_update()
+	  end,
+  }
+
+  use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+end)
+
